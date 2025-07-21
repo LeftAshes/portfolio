e54ed2c62131c8678483be63211b21742878b38d
LeftAshes <cmh315@hanmail.net>
2025-07-21 18:45:39 +0900

    깃허브용 커밋

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // 반드시 저장소 이름으로 설정
+  base: '/portfolio/' // GitHub Pages용 저장소명
 })
