4bf2ea72977c03895dca42ffb40667de23846470
LeftAshes <cmh315@hanmail.net>
2025-07-21 18:20:12 +0900

    깃 허브 페이지용 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+const basename = process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+	<BrowserRouter basename={basename}>
+		<App />
     </BrowserRouter>
   </React.StrictMode>
 )
